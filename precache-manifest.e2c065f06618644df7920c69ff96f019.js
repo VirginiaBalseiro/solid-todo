150349567a01d2afa76f4012cbce74f0eb05bae5
Virginia Balseiro <virginiabalseiro@gmail.com>
2020-10-01 09:39:26 +0200

    Updates

---
+++ precache-manifest.e2c065f06618644df7920c69ff96f019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85f84a5c17913a5e45f4e0ff66ebe7f",
+    "url": "/solid-todo/index.html"
+  },
+  {
+    "revision": "5ea032ace20160d24186",
+    "url": "/solid-todo/static/css/main.92cd2421.chunk.css"
+  },
+  {
+    "revision": "cb6ff51d372aab1d7a98",
+    "url": "/solid-todo/static/js/2.5f805fa3.chunk.js"
+  },
+  {
+    "revision": "b0e4da15454ea95bfa6a54c2663490f7",
+    "url": "/solid-todo/static/js/2.5f805fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea032ace20160d24186",
+    "url": "/solid-todo/static/js/main.db608b38.chunk.js"
+  },
+  {
+    "revision": "03d0df4085fc49bb5d25",
+    "url": "/solid-todo/static/js/runtime-main.7f39d7d2.js"
+  }
+]);
